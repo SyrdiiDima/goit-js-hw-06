e4ec97a5e152edd47c537de47980b3c290b72181
Skladaniuk <skladanyuk1999@gmail.com>
2022-04-26 18:28:16 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const numberOfCategories = document.querySelectorAll("li.item").length;
+console.log(`Number of categories : ${numberOfCategories}`);
+
+
+const categories = document.querySelectorAll(".item");
+categories.forEach((category) => {
+    console.log(`Category: ${category.querySelector("h2").textContent}`);
+console.log(`Elements:${category.querySelectorAll("li").length}`);
+});
+
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,48 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+
+// const liEl = document.createElement('li')
+// console.log(liEl);
+
+// liEl.classList.add('item');
+// console.log(liEl.classList);
+
+
+// liEl.textContent = ingredient;
+
+
+const elements = ingredients.map(ingredient => {
+  
+  const liEl = document.createElement('li');
+  // console.log(liEl);
+  liEl.classList.add('item');
+  liEl.textContent = ingredient;
+  // console.log(liEl.textContent);
+  return liEl;
+})
+document.ul.append(elements);
+console.log(...elements)
+
+
+
+
+
+
+
+
+
+
+// const elements = ingredients.map(ingredient => {
+
+//     const liEl = document.createElement('li');
+//     liEl.classList.add('item');
+//     liEl.textContent = ingredient;
+//   return liEl;
+  
+//   });
+// liEl.append(...elements);
+// console.log(...elements);
