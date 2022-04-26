5275dde030b1d88dff2530ec671754ece0ab1688
Skladaniuk <skladanyuk1999@gmail.com>
2022-04-26 19:08:00 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -5,32 +5,22 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
+  
 ];
-
-
-
-
-// const liEl = document.createElement('li')
-// console.log(liEl);
-
-// liEl.classList.add('item');
-// console.log(liEl.classList);
-
-
-// liEl.textContent = ingredient;
 
 
 const elements = ingredients.map(ingredient => {
   
+  
   const liEl = document.createElement('li');
-  // console.log(liEl);
+
   liEl.classList.add('item');
   liEl.textContent = ingredient;
-  // console.log(liEl.textContent);
-  return liEl;
-})
-document.ul.append(elements);
-console.log(...elements)
+  const ulEl = document.querySelector('ul');
+
+  ulEl.appendChild(liEl)
+
+});
 
 
 
