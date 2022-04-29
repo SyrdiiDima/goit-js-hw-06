335b19043d69c00be6bc2a739c03dc314430a1ec
SyrdiiDima <dimasyrdij@icloud.com>
2022-04-29 14:10:28 +0300

    completed

--- js/task-08.js
+++ js/task-08.js
@@ -6,48 +6,19 @@ form.addEventListener('submit', onFormSubmit)
 function onFormSubmit(event) {
     event.preventDefault();
     
-    // 	const {
-	// 	elements: {
-	// 		email: { value: email },
-	// 		password: { value: password },
-	// 	},
-	// } = event.currentTarget;
-
-
     const formData = new FormData(event.currentTarget);
-
     console.log(formData);
 
+
     formData.forEach((name, value) => {
-        console.log('name', name);
-        console.log('value', value);
-        form.reset();  
-    })
+        // console.log('name', name);
+        // console.log('value', value);
 
-    !name || !value
-		? alert('Пожалуйста, заполните ВСЕ поля')
-		: console.log({ name: value });
+        if (!name || !value) {
+            alert('Please fill in the empty field ')
+        }
 
-
-}
-
-// const loginFormRef = document.querySelector('.login-form');
-
-// const onFormSubmit = (event) => {
-// 	event.preventDefault();
-
-// 	const {
-// 		elements: {
-// 			email: { value: email },
-// 			password: { value: password },
-// 		},
-// 	} = event.currentTarget;
-
-// 	!email || !password
-// 		? alert('Пожалуйста, заполните ВСЕ поля')
-// 		: console.log({ email: password });
-
-// 	loginFormRef.reset();
-// };
-
-// loginFormRef.addEventListener('submit', onFormSubmit);
+    });
+        form.reset();
+ 
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+
+
+
+buttonChangeColor.addEventListener('click', onButtonChangeColor);
+  
+
+function onButtonChangeColor(event) {
+  // console.log("click the button");
+
+  const createdColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = createdColor;
+  spanEl.textContent = createdColor;
+}
+
+
+
